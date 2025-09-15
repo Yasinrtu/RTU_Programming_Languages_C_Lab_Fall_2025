a9af733a1f733383bf5401aadd667bc70cb9d784
Yasinrtu <yasin.isayev@edu.rtu.lv>
2025-09-15 08:27:54 +0000

    hello changes

--- src/hello.c
+++ src/hello.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   // This is your first C program my friend
   // lets practise it 
   printf("Hello, RTU World from C Lab in 2025!\n");
+  printf("Hello from <Yasin>! \n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
